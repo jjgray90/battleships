7183b7b4f8975c6da1f28e8f2fb14d04b44e9992
Your Name <jjgray90@gmail.com>
2022-07-08 18:20:44 +0100

    randomised computer ship placement

--- main.js
+++ main.js
@@ -2,19 +2,7 @@ const userGameboard = document.querySelector(".game-board--user");
 const computerGameboard = document.querySelector(".game-board--computer");
 const gameSquare = document.querySelector(".game-board__square");
 let gameClock = 0;
-const ships = [5, 4, 3, 3, 2];
-
-const userGame = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
+const ships = [5, 4, 3, 2, 3];
 
 const computerGame = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -28,29 +16,69 @@ const computerGame = [
   [0, 0, 3, 3, 3, 3, 0, 0, 0],
 ];
 
+const userGame = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const placeShips = () => {
+  // loop through array. At each element, place a 3 in a single direction for until loop stops
+  let newUserArray = [...userGame];
+  ships.forEach((ship) => {
+    let rowStart = getRandomInt(9);
+    let colStart = getRandomInt(9);
+    let shipPos = (newUserArray[rowStart][colStart] = ship);
+
+    console.log("Ship" + ship, rowStart, colStart);
+
+    for (let i = ship; i > 0; i--) {
+      console.log(rowStart, colStart);
+      if (rowStart <= ship) {
+        shipPos = newUserArray[rowStart++][colStart] = ship;
+      } else if (colStart <= ship) {
+        shipPos = newUserArray[rowStart][colStart++] = ship;
+      } else if (rowStart >= ship) {
+        shipPos = newUserArray[rowStart--][colStart] = ship;
+      } else if (colStart >= ship) {
+        shipPos = newUserArray[rowStart][colStart--] = ship;
+      } else rowStart = getRandomInt(9);
+    }
+  });
+};
+
 const drawBoard = (player, gameBoard) => {
   gameBoard.innerHTML = "";
-  let check = "";
-  let torpedo = "";
-  console.log(gameClock);
+  let squareClass = "";
   for (let r = 0; r < player.length; r++) {
     for (let c = 0; c < player.length; c++) {
-      if (player === userGame) {
+      if (player === userGame && player[r][c] === 3) {
+        squareClass = "ship";
         check = player[r][c];
-      } else if (player === computerGame && player[r][c] === 4) {
+      } else if (player[r][c] === 4) {
+        squareClass = "hit";
         check = player[r][c];
-        torpedo = "hit";
-      } else if (player === computerGame && player[r][c] === 1) {
+      } else if (player[r][c] === 1) {
         check = player[r][c];
-        torpedo = "miss";
+        squareClass = "miss";
       } else {
-        check = "";
-        torpedo = "";
+        squareClass = "";
+        check = player[r][c];
       }
 
-      gameBoard.innerHTML += `<div class="game-board__square ${torpedo}" id=${
+      gameBoard.innerHTML += `<div class="game-board__square ${squareClass}" id=${
         "s" + r + c
-      }></div>`;
+      }>${check}</div>`;
     }
   }
 
@@ -73,5 +101,6 @@ const fireTorpedo = (event) => {
 };
 
 computerGameboard.addEventListener("click", fireTorpedo);
-window.addEventListener("load", drawBoard(computerGame, computerGameboard));
-window.addEventListener("load", drawBoard(userGame, userGameboard));
+placeShips();
+drawBoard(computerGame, computerGameboard);
+drawBoard(userGame, userGameboard);
