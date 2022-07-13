8a4e6425ca9b680d7f810920dd3c8a1c7aa0d3c0
Your Name <jjgray90@gmail.com>
2022-07-13 17:43:38 +0100

    styling complete

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+const gameContainer = document.querySelector(".game-container");
 const userGameboard = document.querySelector(".game-board--user");
 const computerGameboard = document.querySelector(".game-board--computer");
 const gameSquare = document.querySelector(".game-board__square");
-let gameClock = 0;
+const playButton = document.querySelector(".game-button");
+const modal = document.querySelector(".modal");
+const modalText = document.querySelector(".modal-content__message");
+const modalButton = document.querySelector(".modal-content__button");
+
+let userGameClock = 0;
+let computerGameClock = 0;
+
 let ships = [5, 4, 3, 2, 3];
 let shipCount = 0;
+
+console.log(gameContainer.style.display);
 
 let computerGame = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -17,7 +27,7 @@ let computerGame = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-const userGame = [
+let userGame = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -33,8 +43,16 @@ const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
 
+const displayModal = (message) => {
+  modal.style.display = "flex";
+  modalText.innerHTML = message;
+};
+
+const removeModal = () => {
+  modal.style.display = "none";
+};
+
 const checkShips = (array, idFunc) => {
-  console.log("hello");
   const duplicateMap = new Map();
   for (const item of array) {
     const id = idFunc(item);
@@ -82,8 +100,7 @@ const placeShips = () => {
       });
       break;
     } else checkArr = [];
-    // kill++;
-    // console.log(kill, isUnique);
+
     console.table(checkArr);
   }
 };
@@ -91,30 +108,36 @@ const placeShips = () => {
 const drawBoard = (player, gameBoard) => {
   gameBoard.innerHTML = "";
   let squareClass = "";
+  let shipClass = "";
   for (let r = 0; r < player.length; r++) {
     for (let c = 0; c < player.length; c++) {
       if (player === userGame && player[r][c] === 3) {
         squareClass = "ship";
-        check = player[r][c];
+      } else if (player === userGame && player[r][c] === 4) {
+        squareClass = "hit";
+        shipClass = "ship";
       } else if (player[r][c] === 4) {
         squareClass = "hit";
-        check = player[r][c];
       } else if (player[r][c] === 1) {
-        check = player[r][c];
         squareClass = "miss";
+        shipClass = "";
       } else {
         squareClass = "";
-        check = player[r][c];
+        shipClass = "";
       }
 
-      gameBoard.innerHTML += `<div class="game-board__square ${squareClass}" id=${
+      gameBoard.innerHTML += `<div class="game-board__square ${shipClass}" id=${
         "s" + r + c
-      }></div>`;
+      }><div class=${squareClass}></div></div>`;
     }
   }
+};
 
-  if (gameClock === 17) {
-    alert("End of game!");
+const checkVictory = () => {
+  if (computerGameClock === 17) {
+    setTimeout(() => displayModal("End of game, you lost!"), 200);
+  } else if (userGameClock === 17) {
+    setTimeout(() => displayModal("You Won!"), 200);
   }
 };
 
@@ -124,12 +147,13 @@ const fireTorpedo = (event) => {
 
   if (computerGame[row][column] == 3) {
     computerGame[row][column] = 4;
-    gameClock++;
+    userGameClock++;
   } else if (computerGame[row][column] == 0) {
     computerGame[row][column] = 1;
   }
   drawBoard(computerGame, computerGameboard);
-  setTimeout(() => compTorpedo(), 1000);
+  checkVictory();
+  userGameClock !== 17 ? setTimeout(() => compTorpedo(), 1000) : "";
 };
 
 const compTorpedo = () => {
@@ -138,6 +162,7 @@ const compTorpedo = () => {
 
   if (userGame[row][column] == 3) {
     userGame[row][column] = 4;
+    computerGameClock++;
   } else if (userGame[row][column] == 0) {
     userGame[row][column] = 1;
   }
@@ -145,18 +170,23 @@ const compTorpedo = () => {
   //   userGame[getRandomInt(8)][getRandomInt(8)];
   // }
   drawBoard(userGame, userGameboard);
+  checkVictory();
 };
 const alertHandler = () => {
   if (shipCount === 5) {
-    alert("Place your Battleship! (4 grid squares)");
+    // setTimout(() => alert("Place your Battleship! (4 grid squares)"), 200);
+    displayModal("Place your Battleship! <br> (4 grid squares)");
   } else if (shipCount === 9) {
-    alert("Place your Cruiser! (3 grid squares");
+    displayModal("Place your Cruiser! <br> (3 grid squares)");
   } else if (shipCount === 12) {
-    alert("Place your Submarine! (3 grid squares");
+    displayModal("Place your Submarine! <br> (3 grid squares)");
   } else if (shipCount === 15) {
-    alert("Place your destroyer! (2 grid squares");
+    displayModal("Place your destroyer! <br> (2 grid squares)");
   } else if (shipCount === 17) {
-    alert("Let's play - You shoot first");
+    drawBoard(computerGame, computerGameboard);
+    computerGameboard.style.display = "grid";
+
+    setTimeout(() => displayModal("Let's play - You shoot first"), 500);
   }
 };
 
@@ -186,14 +216,48 @@ const placeUserShips = (event) => {
     userGame[row][column] = 3;
     shipCount++;
     alertHandler();
-  } else alert("You can't place anymore!");
+  } else displayModal("You can't place anymore!");
 
   drawBoard(userGame, userGameboard);
 };
 
-window.addEventListener("load", alert("Place your Carrier! (5 grid squares)"));
+const startGame = () => {
+  computerGameboard.style.display = "none";
+  shipCount = 0;
+  userGameClock = 0;
+  computerGameClock = 0;
+  computerGame = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  userGame = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  placeShips();
+
+  drawBoard(userGame, userGameboard);
+  gameContainer.style.display = "flex";
+  displayModal("Place your Carrier! <br> (5 grid squares)");
+};
+
 userGameboard.addEventListener("click", placeUserShips);
 computerGameboard.addEventListener("click", fireTorpedo);
-placeShips();
-drawBoard(computerGame, computerGameboard);
-drawBoard(userGame, userGameboard);
+playButton.addEventListener("click", startGame);
+modalButton.addEventListener("click", removeModal);
